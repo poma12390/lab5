541ce0a2a177d5d78705a70a553c5835fddd220a
poma12390 <kochnevr21@gmail.com>
2022-04-22 14:41:50 +0300

    doing

--- src/java/lab5/client/ServerCaller.java
+++ src/java/lab5/client/ServerCaller.java
@@ -9,23 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServerCaller {
-    DatagramPacket dp;
-    private final int port = 1290;
-    private final InetAddress host;
+
     private final ServerRunner serverRunner;
     private final ServerReciever serverReciever;
 
     {
-        try {
-            DatagramSocket  ds = new DatagramSocket();
-            host = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            System.out.println("NO HOST");
-            throw new RuntimeException(e);
-        } catch (SocketException e) {
-            System.out.println("Socket exception");
-            throw new RuntimeException(e);
-        }
+
     }
     public ServerCaller(ServerRunner serverRunner, ServerReciever serverReciever) {
         this.serverRunner = serverRunner;
@@ -34,15 +23,14 @@ public class ServerCaller {
 
     public List<Worker> getCollection(){
        // byte[] request = new byte[10];// Вызвать процедуру упаковки команды getWorkers
-        serverRunner.call("getCollectionCommand".getBytes(StandardCharsets.UTF_8));
-        byte[] response = serverReciever.receive();
+        byte[] response = serverReciever.receive("getCollectionCommand".getBytes(StandardCharsets.UTF_8));
         List<Worker> result = new ArrayList<>();// вызвать процедуру распаковки из response в result
         return result;
     }
     public Worker getWorker(){
         byte[] request = new byte[10];
         serverRunner.call("getWorkerCommand".getBytes(StandardCharsets.UTF_8));
-        byte[] response = serverReciever.receive();
+
         Worker bum = new Worker(); // response -> result
         return null;
     }
--- src/java/lab5/client/ServerReciever.java
+++ src/java/lab5/client/ServerReciever.java
@@ -1,8 +1,37 @@
 package lab5.client;
 
-public class ServerReciever {
-    public byte[] receive (){
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 
+public class ServerReciever {
+    DatagramChannel dc; ByteBuffer buf;
+    InetAddress host;
+    int port = 1290;
+    SocketAddress addr;
+    int len;
+    public byte[] receive (byte[] arr){
+        try {
+            host = InetAddress.getLocalHost();
+            addr = new InetSocketAddress(host,port);
+            dc = DatagramChannel.open();
+
+        } catch (UnknownHostException e) {
+            System.out.println("NO HOST");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        buf = ByteBuffer.wrap(arr);
+        try {
+            dc.send(buf, addr);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        len = arr.length;
         return new byte[10];
     }
 
